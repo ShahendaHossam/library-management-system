30bde18bb280612f9fd7970a71e65d18f151a490
shahy99 <shahenda.cs.99@gmail.com>
2023-06-10 13:12:20 +0200

    create student form livewire component

---
+++ app/Http/Livewire/Students/StudentForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Students;
+
+use Livewire\Component;
+
+class StudentForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.students.student-form');
+    }
+}
