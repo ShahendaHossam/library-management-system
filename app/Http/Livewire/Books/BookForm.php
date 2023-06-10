a929046afac2ff92e0a486b63f270092c0b12191
shahy99 <shahenda.cs.99@gmail.com>
2023-06-10 13:10:52 +0200

    create book form livewire component

---
+++ app/Http/Livewire/Books/BookForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Books;
+
+use Livewire\Component;
+
+class BookForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.books.book-form');
+    }
+}
