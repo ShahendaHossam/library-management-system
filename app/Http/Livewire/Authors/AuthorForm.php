6fe8790837989f70dbe09c2dfcddba136cac9cd1
shahy99 <shahenda.cs.99@gmail.com>
2023-06-10 13:59:26 +0200

    add rules variable to validate inputs add save function to store data in database and redirect the user to the list view add mount function to intiate the author model

--- app/Http/Livewire/Authors/AuthorForm.php
+++ app/Http/Livewire/Authors/AuthorForm.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Livewire\Authors;
 
+use App\Models\Author;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class AuthorForm extends Component
 {
+    public Author $author;
+
+    protected $rules = [
+        'author.author_name' => 'nullable|max:255',
+        'author.author_email' => 'nullable|max:255',
+        'author.author_address' => 'nullable|max:255',
+        'author.author_phone' => 'nullable|max:255',
+    ];
+
+    public function save(){
+        $this->validate();
+        $users = Auth::user();
+        $this->author->user_id = $users->id;
+        $this->author->save();
+        return redirect()->route('author.index');
+    }
+
+    public function mount(){
+    $this->author = new Author();
+
+    }
+
     public function render()
     {
         return view('livewire.authors.author-form');
