4c08586e11d70f91d12b0208b54e8e47744b5d3f
shahy99 <shahenda.cs.99@gmail.com>
2023-07-16 20:53:12 +0200

    create publisher list component and set edit function

--- app/Http/Livewire/Publishers/PublisherList.php
+++ app/Http/Livewire/Publishers/PublisherList.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire\Publishers;
 
+use App\Models\Publisher;
 use Livewire\Component;
 
 class PublisherList extends Component
 {
+    public Publisher $publisher;
+    public $editMode = false;
+
+
+    public function edit(Publisher $publisher)
+    {
+        $this->publisher = $publisher;
+        $this->editMode = true;
+        return redirect()->route('publisher.edit', $this->publisher->id);
+    }
+
     public function render()
     {
-        return view('livewire.publishers.publisher-list');
+        $publishers = Publisher::all();
+        return view('livewire.publishers.publisher-list', compact('publishers'));
     }
 }
