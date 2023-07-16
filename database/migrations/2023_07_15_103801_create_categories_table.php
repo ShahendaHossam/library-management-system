3a3157e1bae39233578481ba89fad8f9e4b9d0ea
shahy99 <shahenda.cs.99@gmail.com>
2023-07-16 20:55:05 +0200

    create category migration file

---
+++ database/migrations/2023_07_15_103801_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_name', 255)->nullable();
+            $table->string('category_email', 255)->nullable();
+            $table->string('category_address', 255)->nullable();
+            $table->string('category_phone', 255)->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletesTz('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
