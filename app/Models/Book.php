450aa9429880b1b434923e9b1e872e76ce49db2f
shahy99 <shahenda.cs.99@gmail.com>
2023-07-18 11:25:52 +0200

    create relational model functions to access the foreign key data from a specific model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -14,4 +14,17 @@ class Book extends Model
         'author_id',
         'publisher_id',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
 }
